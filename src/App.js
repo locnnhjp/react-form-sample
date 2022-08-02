977e9ef02bbee62f14cf3822e01f7a188a274243
locnnh <locnnh@sample.com>
2022-08-02 09:10:47 +0700

    add file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MyForm from "./Components/MyForm";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <MyForm />
+        </div>
+    );
+}
+
+export default App;
